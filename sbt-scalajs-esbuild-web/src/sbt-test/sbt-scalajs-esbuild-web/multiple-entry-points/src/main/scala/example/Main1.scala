f6e78b305fbc23e2c77e4c1a57cd1b4f5d8390d3
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-09-23 14:40:45 +0300

    WIP test for multiple entry points

---
+++ sbt-scalajs-esbuild-web/src/sbt-test/sbt-scalajs-esbuild-web/multiple-entry-points/src/main/scala/example/Main1.scala
@@ -0,0 +1,45 @@
+package example
+
+import example.facade.Lodash
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+@js.native
+@JSImport("./styles.css", JSImport.Namespace)
+object Style extends js.Object
+
+@js.native
+@JSImport("./javascript.svg", JSImport.Default)
+object JavascriptLogo extends js.Object
+
+object Main1 {
+  val style = Style
+
+  def main(args: Array[String]): Unit = {
+    document.addEventListener(
+      "DOMContentLoaded",
+      { (_: dom.Event) =>
+        setupUI()
+      }
+    )
+  }
+
+  def setupUI(): Unit = {
+    val img = document.createElement("img")
+    img.setAttribute("src", JavascriptLogo.toString)
+    document.body.append(img)
+    val cssImage = document.createElement("div")
+    cssImage.setAttribute("id", "css-image")
+    document.body.append(cssImage)
+    val h1 = document.createElement("h1")
+    h1.textContent = testString()
+    document.body.append(h1)
+  }
+
+  def testString(): String = {
+    Lodash.toUpper("multiple-entry-points main1 works!")
+  }
+}
---
+++ sbt-scalajs-esbuild-web/src/sbt-test/sbt-scalajs-esbuild-web/multiple-entry-points/src/test/scala/example/MainSpec.scala
@@ -0,0 +1,18 @@
+package example
+
+import org.scalajs.dom.document
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MainSpec extends AnyWordSpec with Matchers {
+
+  "Main" should {
+    "work" in {
+      Main.setupUI()
+
+      document
+        .querySelector("h1")
+        .textContent shouldEqual "BASIC-WEB-PROJECT WORKS!"
+    }
+  }
+}
