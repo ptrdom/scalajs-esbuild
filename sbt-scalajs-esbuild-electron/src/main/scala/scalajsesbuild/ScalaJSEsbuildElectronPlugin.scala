85e62873e0b416fc6c3b756369a98aec2e84aa26
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-11-17 21:47:11 +0200

    Revert "Exclude main and preload scalajs modules from esbuild bundling"
    This reverts commit 8f421cbe408746c43927c2054205a9ddea2e4166.

--- sbt-scalajs-esbuild-electron/src/main/scala/scalajsesbuild/ScalaJSEsbuildElectronPlugin.scala
+++ sbt-scalajs-esbuild-electron/src/main/scala/scalajsesbuild/ScalaJSEsbuildElectronPlugin.scala
@@ -1,36 +1,13 @@
 package scalajsesbuild
 
-import org.scalajs.sbtplugin.Stage
-import sbt.*
-import scalajsesbuild.ScalaJSEsbuildPlugin.autoImport.esbuildBundleEntryPoints
+import sbt.AutoPlugin
+import sbt.Plugins
 
 object ScalaJSEsbuildElectronPlugin extends AutoPlugin {
 
   override def requires: Plugins = ScalaJSEsbuildWebPlugin
 
-  object autoImport {}
+  object autoImport {
 
-  import autoImport.*
-
-  override lazy val projectSettings: Seq[Setting[?]] =
-    inConfig(Compile)(perConfigSettings) ++
-      inConfig(Test)(perConfigSettings)
-
-  private lazy val perConfigSettings: Seq[Setting[?]] =
-    perScalaJSStageSettings(Stage.FastOpt) ++
-      perScalaJSStageSettings(Stage.FullOpt)
-
-  private def perScalaJSStageSettings(stage: Stage): Seq[Setting[?]] = {
-    val stageTask = stage.stageTask
-
-    Seq(
-      stageTask / esbuildBundleEntryPoints := {
-        val entryPoints = (stageTask / esbuildBundleEntryPoints).value
-        entryPoints
-          .filter(entryPoint =>
-            !Set("main", "preload").exists(entryPoint.contains)
-          )
-      }
-    )
   }
 }
