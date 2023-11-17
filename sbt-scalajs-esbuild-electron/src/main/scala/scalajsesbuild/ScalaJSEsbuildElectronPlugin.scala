bb88e8498f9f0bcc0e06373d4393b9e4f048b563
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-11-17 21:47:45 +0200

    Revert "Add task for esbuild entrypoint selection"
    This reverts commit a25bb8042e3386a822a55dd6587131d5c12f8709.

--- sbt-scalajs-esbuild-electron/src/main/scala/scalajsesbuild/ScalaJSEsbuildElectronPlugin.scala
+++ sbt-scalajs-esbuild-electron/src/main/scala/scalajsesbuild/ScalaJSEsbuildElectronPlugin.scala
@@ -1,11 +1,12 @@
 package scalajsesbuild
 
+import org.scalajs.sbtplugin.ScalaJSPlugin
 import sbt.AutoPlugin
 import sbt.Plugins
 
 object ScalaJSEsbuildElectronPlugin extends AutoPlugin {
 
-  override def requires: Plugins = ScalaJSEsbuildWebPlugin
+  override def requires: Plugins = ScalaJSPlugin
 
   object autoImport {
 
